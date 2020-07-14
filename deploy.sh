2acbd3565c08d2ff886daf536aeac42091a17ce0
Chih-Ming Louis Lee <louis@chihminglee>
2020-07-13 22:16:18 -0500

    fix deploy 3

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-get push -f git@github.com:chmlee/markdata-editor.git master:gh-pages
+git push -f git@github.com:chmlee/markdata-editor.git master:gh-pages
 
 cd -
